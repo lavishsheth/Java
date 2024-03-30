77281554ff892a4ecad3fc44d213c5e59c257c84
Lavish Sheth <109017996+lavishsheth@users.noreply.github.com>
2024-03-30 20:49:22 +0530

    c

---
+++ Lecure 5/PrimitiveDatatype.java
@@ -0,0 +1,22 @@
+public class PrimitiveDatatype {
+    public static void main(String args[]){
+        int number=10;
+        System.out.println("here is my number:- "+number);
+
+        float f = 2.0211111111111111111111f;
+        System.out.println("here is my float value: "+f);
+
+        double d = 2.0211111111111111111111;
+        System.out.println("here is my double value: "+d);
+
+        long l = 1234567991011L;
+        System.out.println("here is my long value:- "+l);
+
+        char c = 'A';
+        System.out.println("here is my character value:- "+c);
+
+        boolean b = true;
+        System.out.println("here is my boolean value:- "+b);
+
+    }
+}
