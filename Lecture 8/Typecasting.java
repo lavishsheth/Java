5943990ec2e04566b63f72116b4703f573e55e9f
Lavish Sheth <109017996+lavishsheth@users.noreply.github.com>
2024-04-06 00:33:20 +0530

    c
    c

---
+++ Lecture 8/Typecasting.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Typecasting{
+    public static void main(String args[]){
+      Scanner sc = new Scanner(System.in);
+
+    //   System.out.println("enter value: ");
+    //   float a = sc.nextFloat();
+    //   System.out.println("value of float is: "+a);
+
+    //   int b = (int)(a);
+    //   System.out.println(b);
+
+      long c = 12345678910L;
+      int d= 1234567891;
+      long f = d;
+      System.out.println(f);
+    }
+}
