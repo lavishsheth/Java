e8f231eb28dca45a93b27002a29c3e4c2b13de7b
Lavish Sheth <109017996+lavishsheth@users.noreply.github.com>
2024-04-12 00:22:18 +0530

    c

---
+++ Lecture 14/LogicalOp.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class LogicalOp {
+    public static void main(String args[]){
+    // Scanner sc = new Scanner(System.in);
+    // System.out.println("enter value of 1: ");
+    // int var1 = sc.nextInt();
+
+    // System.out.println("enter value of 2: ");
+    // int var2 = sc.nextInt();
+
+    // System.out.println("enter value of 3: ");
+    // int var3 = sc.nextInt();
+
+
+    //And Op:
+    // if((var1>var2)&&(var1>var3)){
+    //     System.out.println("a is greater");
+    // }
+    // else{
+    //     System.out.println("your condition is wrong");
+    // }
+
+    // //OR op:
+    // if((var1>var2)||(var1>var3)){
+    //     System.out.println("a is greater");
+    // }
+    // else{
+    //     System.out.println("your condition is wrong");
+    // }
+
+    //Not op
+    Boolean b =true;
+    if(b){
+        System.out.println("hello");
+       // b=!b;
+    }
+
+    if(b){
+        System.out.println("hi");
+    }
+    }
+}
